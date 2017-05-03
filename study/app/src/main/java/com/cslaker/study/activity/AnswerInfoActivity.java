f58fa5fcc3b9a7ef3ed6b5d50d153fffbc17a2ce
CSLaker <675121663@qq.com>
2017-05-03 12:58:57 +0800

    完善所有菜单项；改变图标样式；细节优化

--- study/app/src/main/java/com/cslaker/study/activity/AnswerInfoActivity.java
+++ study/app/src/main/java/com/cslaker/study/activity/AnswerInfoActivity.java
@@ -3,6 +3,8 @@ package com.cslaker.study.activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.cslaker.study.R;
@@ -17,13 +19,29 @@ public class AnswerInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        toolbar.setTitle("答案");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        menu .findItem(R.id.ab_save).setVisible(false);
+        menu .findItem(R.id.ab_search).setVisible(false);
+        menu .findItem(R.id.ab_edit).setVisible(false);
+        menu .findItem(R.id.ab_share).setVisible(true);
+        menu .findItem(R.id.ab_more).setVisible(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
