2d583b2dbc3aa124e72d35e88d09e4a854b1089d
CSLaker <675121663@qq.com>
2017-05-09 22:51:23 +0800

    示例项内容更新

--- study/app/src/main/java/com/cslaker/study/activity/QuestionInfoActivity.java
+++ study/app/src/main/java/com/cslaker/study/activity/QuestionInfoActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -33,12 +35,7 @@ public class QuestionInfoActivity  extends AppCompatActivity implements View.OnC
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        toolbar.setTitle("问题");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -65,19 +62,69 @@ public class QuestionInfoActivity  extends AppCompatActivity implements View.OnC
     }
 
     private void initDatas() {
-        Answer answer = new Answer();
-        answer.setUser("华仔");
-        answer.setContens("可导一定连续,连续不一定可导  证明：（1）可导一定连续  设y=f(x)在x0处可导,f'(x0)=A  由可导的充分必要条件有  f(x)=f(x0)+A(x-x0)+o（│...");
-        answer.setTime("2017-01-01");
-        answer.setReplayNumbers(66);
-        answer.setLikeNumbers(999);
-
         mAnswerList = new ArrayList<>();
-        for (int i = 0; i < 20; i ++) {
+        Answer answer1 = new Answer();
+        answer1.setUser("华仔");
+        answer1.setContens("可导一定连续,连续不一定可导  证明：（1）可导一定连续  设y=f(x)在x0处可导,f'(x0)=A  由可导的充分必要条件有  f(x)=f(x0)+A(x-x0)+o（│...");
+        answer1.setTime("2017-01-01");
+        answer1.setReplayNumbers(66);
+        answer1.setLikeNumbers(999);
+        mAnswerList.add(answer1);
+
+        Answer answer2 = new Answer();
+        answer2.setUser("隔壁老王");
+        answer2.setContens("因为函数可导,根据可导的定义有\n" +
+                "limΔy/Δx=A (Δx趋向于0)\n" +
+                "所以\n" +
+                "Δy/Δx=A+α (α是Δx趋向于0时的无穷小)\n" +
+                "从而\n" +
+                "Δy=AΔx+αΔx\n" +
+                "当Δx趋向于0时,显然limΔy=0\n" +
+                "由连续定义有\n" +
+                "函数连续.\n" +
+                "连续未必可导,比如y=|x|在x=0处连续,但左导数=-1,右导数=1,不可导.");
+        answer2.setTime("2017-05-02");
+        answer2.setReplayNumbers(30);
+        answer2.setLikeNumbers(454);
+        mAnswerList.add(answer2);
+
+        Answer answer3 = new Answer();
+        answer3.setUser("李紧");
+        answer3.setContens("因为函数可导,根据可导的定义有\n" +
+                "limΔy/Δx=A (Δx趋向于0)。所以\n" +
+                "Δy/Δx=A+α (α是Δx趋向于0时的无穷小)。从而\n" +
+                "Δy=AΔx+αΔx\n" +
+                "当Δx趋向于0时,显然limΔy=0\n" +
+                "由连续定义有：函数连续.\n" +
+                "连续未必可导,比如y=|x|在x=0处连续,但左导数=-1,右导数=1,不可导.");
+        answer3.setTime("2017-05-02");
+        answer3.setReplayNumbers(30);
+        answer3.setLikeNumbers(454);
+        mAnswerList.add(answer3);
+/*        for (int i = 0; i < 20; i ++) {
             mAnswerList.add(answer);
-        }
+        }*/
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        menu .findItem(R.id.ab_save).setVisible(false);
+        menu .findItem(R.id.ab_search).setVisible(false);
+        menu .findItem(R.id.ab_edit).setVisible(false);
+        menu .findItem(R.id.ab_share).setVisible(true);
+        menu .findItem(R.id.ab_more).setVisible(true);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onClick(View v) {
--- study/app/src/main/java/com/cslaker/study/fragment/HomeFragment.java
+++ study/app/src/main/java/com/cslaker/study/fragment/HomeFragment.java
@@ -77,18 +77,37 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     }
 
     private void initDatas() {
-        Question question = new Question();
-        question.setSubject("公共基础课：高等数学");
-        question.setTitle("如何证明：可导一定连续,连续不一定可导？");
-        question.setContens("(1）可导一定连续  设y=f(x)在x0处可导,f'(x0)=A由可导的充分必要条件有f(x)=f(x0)"
-                + "+A(x-x0)+o（│x-x0│）,当x→x0时,f(x)=f(x0)+o（│x-x0│）(2）再由定理：当x...");
-        question.setAnswerNumbers(66);
-        question.setLikeNumbers(999);
-
         mQuestionList = new ArrayList<>();
-        for (int i = 0; i < 20; i ++) {
+        Question question1 = new Question();
+        question1.setSubject("公共基础：高等数学");
+        question1.setTitle("如何证明：可导一定连续,连续不一定可导？");
+        question1.setContens("(1）可导一定连续  设y=f(x)在x0处可导,f'(x0)=A由可导的充分必要条件有f(x)=f(x0)"
+                + "+A(x-x0)+o（│x-x0│）,当x→x0时,f(x)=f(x0)+o（│x-x0│）(2）再由定理：当x...");
+        question1.setAnswerNumbers(66);
+        question1.setLikeNumbers(999);
+        mQuestionList.add(question1);
+
+        Question question2 = new Question();
+        question2.setSubject("专业选修：计算机网络");
+        question2.setTitle(" TCP/IP的核心思想(理念)是什么？");
+        question2.setContens("TCP/IP的核心思想就是“网络互联”，将使用不同低层协议的异构网络，" +
+                "在传输层、网络层建立一个统一的虚拟逻辑网络，以此来屏蔽所有物理网络的硬件差异，从而实现网络的互联.");
+        question2.setAnswerNumbers(15);
+        question2.setLikeNumbers(156);
+        mQuestionList.add(question2);
+
+        Question question3 = new Question();
+        question3.setSubject("专业必修：Java程序设计");
+        question3.setTitle("面向对象和面向过程的区别？");
+        question3.setContens("面向过程就是分析出解决问题所需要的步骤，然后用函数把这些步骤一步一步实现，使用的时候一个一个依次调用就可以了。 \n" +
+                "面向对象是把构成问题事务分解成各个对象，建立对象的目的不是为了完成一个步骤，而是为了描叙某个事物在整个解决问题的步骤中的行为。");
+        question3.setAnswerNumbers(9);
+        question3.setLikeNumbers(40);
+        mQuestionList.add(question3);
+
+/*        for (int i = 0; i < 20; i ++) {
             mQuestionList.add(question);
-        }
+        }*/
     }
 
     @Override
