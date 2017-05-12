cc49cdbcfe552a05c44410bda4cfe62ddc7d16a1
CSLaker <675121663@qq.com>
2017-05-12 23:48:09 +0800

    根据Android Lint提示修复一些代码

--- study/app/src/main/java/com/cslaker/study/fragment/GameFragment.java
+++ study/app/src/main/java/com/cslaker/study/fragment/GameFragment.java
@@ -19,8 +19,7 @@ public class GameFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_game, container, false);
-        return view;
+        return inflater.inflate(R.layout.fragment_game, container, false);
     }
 
     public GameFragment(){}
